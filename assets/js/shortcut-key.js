a70edfb4c22717281698a8c0754b9958e1c0d763
tristanaverty <tristan.averty@gmail.com>
2025-03-16 20:48:16 +0100

    New layout (updates)

--- assets/js/shortcut-key.js
+++ assets/js/shortcut-key.js
@@ -5,7 +5,7 @@ document.addEventListener("readystatechange", () => {
     let shortcutKeyElement = document.querySelector("#search-toggle .nav-link");
     if (shortcutKeyElement && isMac) {
       // use the unicode for command key
-      shortcutKeyElement.innerHTML = '&#x2318; k <i class="ti ti-search"></i>';
+      shortcutKeyElement.innerHTML = '&#128270;<i class="ti ti-search"></i>';
     }
   }
 });
